7ad8642559c1ea9b138f51e682a8bce13f3500b2
onlytarg <onlytarg@example.com>
2021-02-26 10:15:07 +0200

    small fixes

---
+++ lib/models/news_item.dart
@@ -0,0 +1,13 @@
+class NewsItem {
+  final String title;
+  final String description;
+  final String url;
+  final String imageUrl;
+
+  NewsItem(
+    this.title,
+    this.description,
+    this.url,
+    this.imageUrl,
+  );
+}
---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class User {
+  final String firstName;
+
+  final String lastName;
+
+  final String email;
+
+  User({
+    this.email,
+    this.firstName,
+    this.lastName,
+  });
+}
