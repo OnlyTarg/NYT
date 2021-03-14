99e6c2d9f8dd14d6adf49d47ad654a7486e89fa2
onlytarg <onlytarg@example.com>
2021-03-14 15:38:59 +0200

    small changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive/hive.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:nyt_app/presentation/nyt_app.dart';
 
 import 'package:path_provider/path_provider.dart';
@@ -13,29 +12,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = SimpleBlocObserver();
   await Firebase.initializeApp();
-  myBackgroundMessageHandler();
   final appDocumentDir = await getApplicationDocumentsDirectory();
   Hive.init(appDocumentDir.path);
   runApp(NYTApp());
-}
-
-Future<dynamic> myBackgroundMessageHandler() {
-  final fbm = FirebaseMessaging();
-  fbm.requestNotificationPermissions();
-  fbm.configure(
-    onResume: (message) {
-      print(message);
-      return;
-    },
-    onMessage: (message) {
-      print(message);
-      return;
-    },
-    onLaunch: (message) {
-      print(message);
-      return;
-    },
-  );
-
-  // Or do other work.
 }
